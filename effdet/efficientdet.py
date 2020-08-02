0b36cc1cccfe92febc64f6eb569ca4393bd73964
Ross Wightman <rwightman@gmail.com>
2020-08-01 17:34:33 -0700

    Update feature_info usage to support upcoming timm changes with feature support for all networks

--- effdet/efficientdet.py
+++ effdet/efficientdet.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 import logging
 import math
 from collections import OrderedDict
-from typing import List
+from typing import List, Callable
 
 from timm import create_model
 from timm.models.layers import create_conv2d, drop_path, create_pool2d, Swish, get_act_layer
@@ -440,6 +440,17 @@ def _init_weight_alt(m, n='', ):
         m.bias.data.zero_()
 
 
+def get_feature_info(backbone):
+    if isinstance(backbone.feature_info, Callable):
+        # old accessor for timm versions <= 0.1.30, efficientnet and mobilenetv3 and related nets only
+        feature_info = [dict(num_chs=f['num_chs'], reduction=f['reduction'])
+                        for i, f in enumerate(backbone.feature_info())]
+    else:
+        # new feature info accessor, timm >= 0.2, all models supported
+        feature_info = backbone.feature_info.get_dicts(keys=['num_chs', 'reduction'])
+    return feature_info
+
+
 class EfficientDet(nn.Module):
 
     def __init__(self, config, norm_kwargs=None, pretrained_backbone=True, alternate_init=False):
@@ -448,8 +459,7 @@ class EfficientDet(nn.Module):
         self.backbone = create_model(
             config.backbone_name, features_only=True, out_indices=(2, 3, 4),
             pretrained=pretrained_backbone, **config.backbone_args)
-        feature_info = [dict(num_chs=f['num_chs'], reduction=f['reduction'])
-                        for i, f in enumerate(self.backbone.feature_info())]
+        feature_info = get_feature_info(self.backbone)
 
         act_layer = get_act_layer(config.act_type)
         self.fpn = BiFpn(config, feature_info, norm_kwargs=norm_kwargs, act_layer=act_layer)
